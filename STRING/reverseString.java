f79fc1088d8b81b5f54fbd409e877887b885dec5
akash <akhouriakash10@gmail.com>
2025-08-14 22:21:29 +0530

    second commit

---
+++ STRING/reverseString.java
@@ -0,0 +1,15 @@
+package STRING;
+
+public class reverseString {
+
+    public void reverseStr(char[] s){
+
+        for(int i=0;i<s.length/2;i++){
+            char temp = s[i];
+            s[i] = s[s.length-i-1];
+        s[s.length-i-1] = temp;
+
+        }
+    }
+    
+}
